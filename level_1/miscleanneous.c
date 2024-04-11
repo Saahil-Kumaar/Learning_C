8fb40a8f4ff1724f780af6fbc142faecaa9d4e16
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2024-04-11 22:50:46 +0530

    Delete level_1 directory

--- level_1/miscleanneous.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    int a=3;
-    printf("%d %d %d",a,++a,a++); //code goes from right to left inside a line.
-    return 0;
-}
--- level_1/random_num.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-int main(){
-    int number;
-    srand(time(0));
-    number=rand();
-    printf("%d\n",number);
-}
