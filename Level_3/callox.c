371ea90691f754b7b6e912fac3f37f7807648c1d
Sahil Kumar <166407765+Saahil-Kumaar@users.noreply.github.com>
2024-04-20 11:29:36 +0530

    Update callox.c

--- Level_3/callox.c
+++ Level_3/callox.c
@@ -2,7 +2,7 @@
 #include<stdlib.h>
 int main(){         
     float *ptr;         
-    ptr=(float *) calloc(30,sizeof(float));   //will allocate memory for 6 integers
+    ptr=(float *) calloc(30,sizeof(float));   //will allocate memory for 6 integers and initiallize each byte with value 0 unlike malloc
     free(ptr);
     return 0;
 }
